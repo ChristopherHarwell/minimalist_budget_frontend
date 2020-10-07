45e6995ec21fc7f26125de08d37a5b91cd2ce3ee
Chris Harwell <chrisharwell72@gmail.com>
2020-10-07 01:58:05 -0400

    added TODO

--- src/pages/BudgetReport.js
+++ src/pages/BudgetReport.js
@@ -6,7 +6,7 @@ import { Link } from "react-router-dom";
 export const BudgetReport = () => {
   return (
     <>
-    {/* Add react vis XY Plot */}
+    {/* TODO Add react vis XY Plot */}
       <Link to="/budget-report">
         <IncomingFinances />
         <OutgoingExpenses />
