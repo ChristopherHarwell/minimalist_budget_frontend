3ef2d5cccd3ee24733e01c44902d679ef739d124
Chris Harwell <chrisharwell72@gmail.com>
2020-10-07 01:52:13 -0400

    Added  link to BudgetReport

--- src/pages/BudgetReport.js
+++ src/pages/BudgetReport.js
@@ -1,12 +1,16 @@
 import React from "react";
 import { IncomingFinances } from "../components/incomingFinances/IncomingFinances";
 import { OutgoingExpenses } from "../components/outgoingExpenses/OutgoingExpenses";
+import { Link } from "react-router-dom";
 
 export const BudgetReport = () => {
   return (
     <>
-      <IncomingFinances />
-      <OutgoingExpenses />
+    {/* Add react vis XY Plot */}
+      <Link to="/budget-report">
+        <IncomingFinances />
+        <OutgoingExpenses />
+      </Link>
     </>
   );
 };
