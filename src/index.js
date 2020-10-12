41ea0fffdb56a960bda6dcf99c4f9af0efde2d8d
Chris Harwell <chrisharwell72@gmail.com>
2020-10-12 18:00:21 -0400

    added redux store

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
-import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+import React from "react";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import rootReducer from "./reducers";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import { BrowserRouter as Router } from "react-router-dom";
+
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
+  <Provider>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
   document.getElementById("root")
 );
 
