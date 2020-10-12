33e6d4619daf0a224b9ed9e2a1022bcd5fe510e4
Chris Harwell <chrisharwell72@gmail.com>
2020-10-12 19:50:59 -0400

    updated route in server.js

--- api/server.js
+++ api/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
 
+const BudgetRouter = require("../budget/expenses.router.js");
+
 const server = express();
 
 server.use(helmet());
@@ -11,5 +13,7 @@ server.use(express.json());
 server.get("/", (_, res) => {
     res.status(200).json({ api: "up" });
   });
-  
+
+  server.use("/api/budget", BudgetRouter);
+
 module.exports = server;
